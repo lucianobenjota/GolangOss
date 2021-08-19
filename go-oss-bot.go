94016b24605663b577282edf4e0ef0a626a15837
lucianobenjota <elluciano22@gmail.com>
2021-08-19 12:57:19 -0300

    teclado

--- go-oss-bot.go
+++ go-oss-bot.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -11,11 +12,18 @@ import (
 
 func main() {
   err := godotenv.Load()
+
   if err != nil {
     log.Fatal("Error al leer variables de entorno")
   }
 
   tgApi := os.Getenv("TELEGRAM_API_KEY")
+  tgUser := os.Getenv("TELEGRAM_USER_ID")
+
+  userId, err := strconv.ParseInt(tgUser, 10, 64)
+  if err != nil {
+    log.Fatal("Error al leer variables de entorno")
+  }
 
 	b, err := tb.NewBot(tb.Settings{
 		// You can also set custom API URL.
@@ -29,9 +37,35 @@ func main() {
 		return
 	}
 
+  var (
+    menu = &tb.ReplyMarkup{ResizeReplyKeyboard: true, OneTimeKeyboard: true}
+    btnCompañia = menu.Text("🏢 Compañia")
+    btnAyuda = menu.Text(" Ayuda")
+  )
+
+  menu.Reply(
+    menu.Row(btnCompañia),
+    menu.Row(btnAyuda),
+  )
+
   b.Handle("/start", func(m *tb.Message){
-    b.Send(m.Sender, "Bienvenido al megabot")
+
+    if m.Chat.ID != userId{
+      log.Printf("El id %v envió un mensaje", m.Chat.ID)
+      return
+    }
+
+    b.Send(m.Sender, "Bienvenido al megabot", menu)
     log.Printf("id: %v", m.Chat.ID)
+  })
+
+
+  b.Handle(&btnCompañia, func(m *tb.Message){
+    if m.Chat.ID != userId{
+      return
+    }
+    b.Send(m.Sender, "Envie el archivo de Reporte de compañia")
+    b.Delete(m)
   })
 
 	b.Start()
