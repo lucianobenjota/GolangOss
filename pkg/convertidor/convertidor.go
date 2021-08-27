87e68871589af8785e6a7058d8cf98c66fa0cc3b
lucianobenjota <elluciano22@gmail.com>
2021-08-27 12:03:34 -0300

    convertir archivo a csv

---
+++ pkg/convertidor/convertidor.go
@@ -0,0 +1,27 @@
+package convertidor
+
+import (
+	"os"
+	"os/exec"
+)
+
+func CmdWrapper(xlsFile string, outCSVFile string) error {
+	// LLamar al comando de rust para convertir el xls a csv
+	if _, err := os.Stat(outCSVFile); os.IsExist(err) {
+		os.Remove(xlsFile)
+	}
+
+	cmd := exec.Command("xls2csv", xlsFile)
+	csvFile, err := os.Create(outCSVFile)
+	if err != nil {
+		return err
+	}
+	defer csvFile.Close()
+
+	cmd.Stdout = csvFile
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
