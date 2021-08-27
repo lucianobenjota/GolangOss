7785a7d02c54a3f3d23a9ee20ecade3d7c831ad6
lucianobenjota <elluciano22@gmail.com>
2021-08-27 11:54:29 -0300

    convertir archivos de reporte de compania

--- pkg/compania/compania.go
+++ pkg/compania/compania.go
@@ -1,91 +1,138 @@
 package compania
 
 import (
-	"log"
+	"encoding/csv"
+	"io"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strconv"
 	"strings"
 
-	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
+	"github.com/gocarina/gocsv"
+	"github.com/lucianobenjota/go-oss-bot/m/pkg/convetidor"
 )
 
-
-
-func ArchivoACsv(rutaXLS string) (rutaCSV string, err error){
-  nombreXLS := filepath.Base(rutaXLS)
-  ext := filepath.Ext(rutaXLS)
-  nombreCSV := strings.TrimSuffix(nombreXLS, ext) + ".csv"
-
-  directorio := filepath.Dir(rutaXLS)
-  rutaCSV = filepath.Join(directorio, nombreCSV)
-  log.Printf("ruta del archivo a convertir: %v", rutaXLS)
-  log.Printf("ruta de salida: %v", rutaCSV)
-  cmd := exec.Command("ssconvert", rutaXLS, rutaCSV)
-  log.Printf("Convirtiendo archivo a csv..")
-  err = cmd.Run()
-  if err != nil {
-    return "", err
-  }
-  return rutaCSV, nil
+type Afiliado struct {
+	Numero         string `csv:"Nro. Afiliado"`
+	DNI            string `csv:"Nro. Doc."`
+	Nombre         string `csv:"Afiliado"`
+	Parentesco     string `csv:"Tipo Afiliado"`
+	FechaNac       string `csv:"Fecha Nac."`
+	FechaAlta      string `csv:"Fecha Alta"`
+	Localidad      string `csv:"Localidad"`
+	Provincia      string `csv:"Provincia"`
+	Telefono       string `csv:"Teléfono"`
+	Celular        string `csv:"Celular"`
+	TipoDNI        string `csv:"Tipo Doc."`
+	Edad           string `csv:"Edad"`
+	EstadoAfiliado string `csv:"Estado Actual"`
+	NuevoTelefono  string `csv:"-"`
 }
 
-func LeerCSV(rutaCSV string) (df dataframe.DataFrame, err error){
-  csvFile, err := os.Open(rutaCSV)
-  if err != nil {
-    return dataframe.DataFrame{}, err
-  }
-
-  df = dataframe.ReadCSV(csvFile, dataframe.DefaultType(series.String))
-  log.Println("df: ", df)
-  return df, nil
+type Salida struct {
+	Numero     string `csv:"numero"`
+	Nrodoc     string `csv:"nrodoc"`
+	Nombre     string `csv:"nombre"`
+	Parentesco string `csv:"parentesco"`
+	FechaNac   string `csv:"fechanac"`
+	FechaAlta  string `csv:"fecha_alta"`
+	Localidad  string `csv:"localidad"`
+	Provincia  string `csv:"prov_nombre"`
+	Telefono   string `csv:"telefono"`
+	TipoDNI    string `csv:"tipodoc"`
+	Edad       string `csv:"edad"`
 }
 
-func ProcesarXLS(rutaXLS string) (df dataframe.DataFrame, err error){
-  var rutaCSV string
-  rutaCSV, err = ArchivoACsv(rutaXLS )
-  if err != nil {
-    return dataframe.DataFrame{}, err
-  }
-  df, err = LeerCSV(rutaCSV)
+var interCSV string
 
-  trim := func(s series.Series) series.Series {
-    strs := s.String()
-    var str string
-    for _, f := range strs{
-      str = strings.Trim(strconv.QuoteRune(f), " ") + " caca"
-    }
-    return series.Strings(str)
-  }
+func ReporteACSV(reportPath string, outCSVPath string) error {
+	interCSV = "archivo.csv"
+	// Crear un archivo intermedio,  podria ser reemplazado por uno temporal
+	err := convetidor.CmdWrapper(reportPath, interCSV)
+	csvInterFile, err := os.Open(interCSV)
+	if err != nil {
+		return err
+	}
 
-  df = df.Capply(trim)
-  df = df.Rapply(trim)
+	defer csvInterFile.Close()
+	err = ProcesarReporte(csvInterFile, outCSVPath)
+	if err != nil {
+		return err
+	}
 
-  return df, nil
+	err = DeleteIntermediateCSV(interCSV)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func GrabarCSV(rutaCSV string, df dataframe.DataFrame) (err error){
-  f, err := os.Open(rutaCSV)
-  if err != nil {
-    log.Fatal(err)
-  }
-  df.WriteCSV(f)
-  return nil
+func ProcesarReporte(csvFile *os.File, csvOutPath string) error {
+	// Procesar el reporte de compañia en formato csv
+
+	empadronamiento := []*Afiliado{}
+
+	if err := gocsv.UnmarshalFile(csvFile, &empadronamiento); err != nil {
+		return err
+	}
+
+	salida := []*Salida{}
+
+	for _, af := range empadronamiento {
+		res := Salida{}
+		af.Telefono = strings.TrimSpace(af.Telefono)
+		af.Celular = strings.TrimSpace(af.Celular)
+		af.NuevoTelefono = getTelefono(af.Telefono, af.Celular)
+		af.EstadoAfiliado = strings.TrimSpace(af.EstadoAfiliado)
+		if af.EstadoAfiliado == "Activo" {
+			res.Numero = strings.TrimSpace(af.Numero)
+			res.Nrodoc = strings.TrimSpace(af.DNI)
+			res.Nombre = strings.TrimSpace(af.Nombre)
+			res.Parentesco = strings.TrimSpace(af.Parentesco)
+			res.FechaNac = strings.TrimSpace(af.FechaNac)
+			res.FechaAlta = strings.TrimSpace(af.FechaAlta)
+			res.Localidad = strings.TrimSpace(af.Localidad)
+			res.Provincia = strings.TrimSpace(af.Provincia)
+			res.Telefono = strings.TrimSpace(af.NuevoTelefono)
+			res.TipoDNI = strings.TrimSpace(af.TipoDNI)
+			res.Edad = strings.TrimSpace(af.Edad)
+			salida = append(salida, &res)
+		}
+	}
+
+	gocsv.SetCSVWriter(func(out io.Writer) *gocsv.SafeCSVWriter {
+		writer := csv.NewWriter(out)
+		writer.Comma = ';'
+		writer.UseCRLF = true
+		return gocsv.NewSafeCSVWriter(writer)
+	})
+
+	csvOutFile, err := os.OpenFile(csvOutPath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+
+	if err != nil {
+		return err
+	}
+
+	err = gocsv.MarshalFile(&salida, csvOutFile)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func XLSaCSV(rutaXLS string, rutaCSV string) (err error){
-  var df dataframe.DataFrame
-  df, err = ProcesarXLS(rutaXLS)
-  if err != nil {
-    log.Fatal(err)
-    return err
-  }
-  err = GrabarCSV(rutaCSV, df)
-  if err != nil {
-    log.Fatal(err)
-    return err
-  }
-  return nil
+func getTelefono(telefono string, celular string) string {
+	// Si existe el celular devuelve el celular si no devuelvel el telefono
+	if len(celular) > 0 {
+		return celular
+	}
+	return telefono
+}
+
+func DeleteIntermediateCSV(csvPath string) error {
+	err := os.Remove(csvPath)
+	if err != nil {
+		return err
+	}
+	return nil
 }
