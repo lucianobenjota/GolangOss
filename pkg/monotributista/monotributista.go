6da54e5cb844563e9a1ae98548439afeb2cd1e7e
lucianobenjota <elluciano22@gmail.com>
2022-01-25 10:19:59 -0300

    acciones basicas de monotributos

---
+++ pkg/monotributista/monotributista.go
@@ -0,0 +1,61 @@
+package monotributista
+
+import (
+	"database/sql"
+	"log"
+	"time"
+)
+
+// Pago de monotributista
+type Pago struct {
+	id int
+	cuit string
+	fecha time.Time
+	periodo time.Time
+}
+
+// Monotributista
+type Mononotributista struct {
+	id int
+	cuit string
+	nombre string
+	estado string
+}
+
+// Inserta un nuevo monotributista en la base de datos
+func NuevoMonotributista(db *sql.DB, mono Mononotributista) {
+	stmt, _ := db.Prepare("INSERT INTO monotributistas (id, cuit, nombre, estado) VALUES (?, ?, ?, ?)")
+	stmt.Exec(nil, mono.cuit, mono.nombre, mono.estado)
+	defer stmt.Close()
+	log.Printf("se agrego monotributita con el cuit %s", mono.cuit)
+}
+
+// Listar monotributistas
+func ListarMonotributistas(db *sql.DB) []Mononotributista {
+	rows, _ := db.Query("SELECT * FROM monotributistas")
+	defer rows.Close()
+
+	err := rows.Err()
+	if err != nil {
+		log.Panic("Error al consultar monotributistas")
+	}
+
+	monos := make([]Mononotributista, 0)
+
+	for rows.Next() {
+		mono := Mononotributista{}
+		err = rows.Scan(&mono.id, &mono.cuit, &mono.nombre, &mono.estado)
+		if err != nil {
+			log.Panic("Error al consultar monotributista")
+		}
+
+		monos = append(monos, mono)
+	}
+
+	err =  rows.Err()
+	if err != nil {
+		log.Panicln("Error al consultar monotributistas")
+	}
+	
+	return monos
+}
